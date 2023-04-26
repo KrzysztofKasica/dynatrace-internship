4def02872840b008e606a9aeb200bf0697921de8
unknown <krzysztof.kasica@o2.pl>
2023-04-26 10:50:33 +0200

    [ADD] finished the app page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,34 +2,29 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
+  Center,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { AvgExchangeRate } from "./components/AvgExchangeRate"
+import { MaxMinValue } from "./components/MaxMinValue"
+import { BuyAskDiff } from "./components/BuyAskDiff"
+
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
+        <Center mt="-1000">
+          <VStack spacing={8}>
+              <AvgExchangeRate />
+              <MaxMinValue />
+              <BuyAskDiff />
+          </VStack>
+        </Center>
       </Grid>
     </Box>
   </ChakraProvider>
